8012fc6f82931b71705ecc3d1264f230994cf50e
Caelr <rachidijudicael@gmail.com>
2024-10-03 15:44:37 +0400

    currying

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,20 @@ console.log(incrementToString(3))
 
 const compose: Compose = (f, g) => x => f(g(x))
 
-const cIncrementToString = compose(toString, increment)
-console.log(cIncrementToString(3))
+const cpIncrementToString = compose(toString, increment)
+console.log(cpIncrementToString(3))
+
+// Currying
+const nSum: NSum = (a, b) => a + b
+console.log(nSum(1, 2))
+
+const sum: Sum = (a) => (b) => a + b
+const addOne: Increment = sum(1)
+console.log(addOne(2))
+const decrement: Increment = sum(-1)
+console.log(decrement(2))
+
+const curry2: Curry2 = f => a => b => f(a, b)
+
+const sum2 = curry2(nSum)
+console.log(sum2(1)(2))
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,8 @@
 type Increment = (n: number) => number
 type ToString = (n: number) => string
 type IncrementToString = (n: number) => string
+type NSum = (a: number, b: number) => number
+type Sum = (a: number) => (b: number) => number
+
 type Compose = <A, B, C>(f: (n: B) => C, g: (n: A) => B) => (n: A) => C
+type Curry2 = <A, B, Z>(f: (a: A, b: B) => Z) => (a: A) => (b: B) => Z
